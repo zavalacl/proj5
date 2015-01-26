5b24c01f1d1456505cc91a0009d23149b3b3f1df
Claudia Zavala <zavalacl@msu.edu>
2015-01-26 11:47:02 -0500

    initial commit

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 class MyWebApp < Sinatra::Base
 	get '/' do 
-		"Welcome to the webapp of DOOM, where the copyright is always up to date! Copyright 2014-#{ Time.now.year } Chris Fritz."
+		"Welcome to the webapp of DOOM, where the copyright is always up to date! Copyright 2014-#{ Time.now.year } Claudia Zavala."
 	end
 		
 end
